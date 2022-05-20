98089c45cab3aaca643f9d5db0231043e69399ae
Oskar Sjölund <oskar.sjolund93@gmail.com>
2022-05-20 17:07:31 +0300

    tests for keygenutils

---
+++ src/tests/key_gen_utils_test.py
@@ -0,0 +1,15 @@
+import unittest
+from sympy import primerange
+from src.key_gen_utils import KeyGenUtilities
+
+class TestKeyGenUtilities(unittest.TestCase):
+    def setUp(self):
+        self.keygen_utils = KeyGenUtilities()
+
+    def test_constructor_creates_keygen_utils(self):
+        self.assertSetEqual(self.keygen_utils.low_primes, set())
+
+    def test_find_all_primes_gives_correct_primes(self):
+        self.keygen_utils.find_all_primes(8000)
+        primes = set(primerange(0, 8000))
+        self.assertSetEqual(self.keygen_utils.low_primes, primes)
