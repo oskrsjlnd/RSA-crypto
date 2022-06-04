dea56bbc6f6213d7c8c638ace9e61bbc505175b3
Oskar Sjölund <oskar.sjolund93@gmail.com>
2022-06-04 23:48:47 +0300

    fixed tests

--- src/tests/key_gen_utils_test.py
+++ src/tests/key_gen_utils_test.py
@@ -18,16 +18,16 @@ class TestKeyGenerator(unittest.TestCase):
         self.assertNotEqual(prime_a, prime_b)
     
     def test_prime_test_round_returns_true_for_prime(self):
-        verified_prime_1 = """249985679617749176375950878529972584286629012057
-        5138470858067744571954165845818765869202092388075475424521345616052888
-        727535565798781930182410281399742821"""
-        self.assertTrue(self.keygen.prime_test_round(int(verified_prime_1)))
+        verified_prime = 2499856796177491763759508785299725842866290120575138470
+        858067744571954165845818765869202092388075475424521345616052888727535565
+        798781930182410281399742821
+        self.assertTrue(self.keygen.prime_test_round(verified_prime))
     
     def test_prime_test_round_returns_false_for_composite(self):
-        composite = """11794051520578639372735469069180758408079391665362167045
-        75173631165760486340876510104699932282135334535738762728098755987028952
-        0921156781759163203476682792"""
-        self.assertFalse(self.keygen.prime_test_round(int(composite)))
+        composite = 1179405152057863937273546906918075840807939166536216704575173
+        6311657604863408765101046999322821353345357387627280987559870289520921156
+        781759163203476682792
+        self.assertFalse(self.keygen.prime_test_round(composite))
     
     def test_generate_prime_returns_pseudoprime(self):
         pseudoprime = self.keygen.generate_prime()
@@ -37,5 +37,5 @@ class TestKeyGenerator(unittest.TestCase):
         self.keygen.find_distinct_primes()
         a = 45220
         b = 45530
-        gcd = self.keygen.extended_euclidean_algorithm(a, b)
-        self.assertEqual(gcd, 10)
+        self.keygen.extended_euclidean_algorithm(a, b)
+        self.assertEqual(self.keygen.get_gcd(), 10)
