ba1556c662df7ba230b81b7bda79e87642d8b209
Oskar Sjölund <oskar.sjolund93@gmail.com>
2022-06-05 15:11:13 +0300

    clearer variable names

--- src/key_gen_utils.py
+++ src/key_gen_utils.py
@@ -17,11 +17,11 @@ class KeyGenerator:
     
     def exponent_for_prime_test(self, prime_candidate):
         d = prime_candidate - 1
-        max_div_by_two = 0
+        max_divisions = 0
         while d ^ 1 == d + 1:
             d >>= 1
-            max_div_by_two += 1
-        return (d, max_div_by_two)
+            max_divisions += 1
+        return (d, max_divisions)
 
     def generate_prime(self, bits=1024, rounds=40):
         while True:
@@ -42,7 +42,7 @@ class KeyGenerator:
         a = randrange(2, prime_candidate-1)
         d = exp_and_max_div[0]
         max_div_by_two = exp_and_max_div[1]
-        
+
         x = pow(a, d, prime_candidate)
         if x == 1 or x == prime_candidate - 1:
             return True
@@ -54,18 +54,18 @@ class KeyGenerator:
         return False
     
     def extended_euclidean_algorithm(self, a, b):
-        old_r, r = a, b
-        old_s, s = 1, 0
-        old_t, t = 0, 1
+        gcd, remainder = a, b
+        coefficient_1, s = 1, 0
+        coefficient_2, t = 0, 1
 
-        while r != 0:
-            quotient = old_r//r
-            old_r, r = r, old_r-quotient*r
-            old_s, s = s, old_s-quotient*s
-            old_t, t = t, old_t-quotient*t
+        while remainder != 0:
+            quotient = gcd//remainder
+            gcd, remainder = remainder, gcd-quotient*remainder
+            coefficient_1, s = s, coefficient_1-quotient*s
+            coefficient_2, t = t, coefficient_2-quotient*t
         
-        self.coefficients = (old_s, old_t)
-        self.gcd = old_r
+        self.coefficients = (coefficient_1, coefficient_2)
+        self.gcd = gcd
 
     def get_gcd(self):
         return self.gcd
