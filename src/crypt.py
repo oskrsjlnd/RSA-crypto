8ee2273e94a4f0709201e67cd392d843ea5d77d9
Oskar Sjölund <oskar.sjolund93@gmail.com>
2022-06-12 14:51:38 +0300

    catch exceptions

--- src/crypt.py
+++ src/crypt.py
@@ -2,17 +2,23 @@ class Crypt:
     def encrypt_message(self, plaintext, public_key):
         n = public_key[0]
         e = public_key[1]
-        bytesize = self.size_as_bytes_for_conversion(plaintext)
-        plaintext_as_int = self.str_to_int(plaintext)
-        ciphertext = pow(plaintext_as_int, e, n)
+        try:
+            bytesize = self.size_as_bytes_for_conversion(plaintext)
+            plaintext_as_int = self.str_to_int(plaintext)
+            ciphertext = pow(plaintext_as_int, e, n)
+        except:
+            return None
         return ciphertext, bytesize
 
     def decrypt_message(self, cipher, private_key):
         n = private_key[0]
         d = private_key[1]
-        ciphertext, size_for_conversion = cipher[0], cipher[1]
-        plaintext_as_int = pow(ciphertext, d, n)
-        plaintext = self.int_to_str(plaintext_as_int, size_for_conversion)
+        try:
+            ciphertext, size_for_conversion = cipher[0], cipher[1]
+            plaintext_as_int = pow(ciphertext, d, n)
+            plaintext = self.int_to_str(plaintext_as_int, size_for_conversion)
+        except:
+            return None
         return plaintext
     
     def str_to_int(self, text):
