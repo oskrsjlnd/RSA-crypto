b6f6a72ebf5080143d4279274ebd53d2eeaf6df0
Oskar Sjölund <oskar.sjolund93@gmail.com>
2022-06-20 22:13:13 +0300

    added commands for user

--- src/ui.py
+++ src/ui.py
@@ -1,11 +1,10 @@
-from src.key_gen_utils import KeyGenerator
-from src.crypto import Crypt
+from key_gen_utils import KeyGenerator
+from crypto import Crypt
 
 class UI:
     def __init__(self, crypt: Crypt, keygen: KeyGenerator):
         self.crypt = crypt
         self.keygen = keygen
-        self.key_pair = None
         self.encrypted_message = None
 
     def launch(self):
@@ -16,9 +15,10 @@ class UI:
     def select_key_bit_size(self):
         print("Select bit size for keys from the following options:\n"
                 "1 - 1024\n"
-                "2 - 2048\n")
+                "2 - 2048")
         while True:
-            key_bits = input("Select 1 or 2 and press Enter: ")
+            self.print_lines()
+            key_bits = input(">>> ")
             if key_bits == "1":
                 key_size = 1024
                 break
@@ -28,72 +28,98 @@ class UI:
                 break
 
             else:
-                print("Invalid input.\n")
-
-        print(f"Key size is {key_size} bits.\n")
+                self.print_lines()
+                print("Select 1 or 2.")
+        self.print_lines()
+        print(f"Keys of {key_size} bits generated.")
         return key_size
 
     def key_generation(self, bits):
-        keys = self.keygen.generate_keys(bits)
-        return keys
+        self.keygen.generate_keys(bits)
 
     def encrypt_user_message(self, public_key):
         while True:
-            message = input("Enter message to encrypt, max length 120"
-            + "characters or type back to choose action again:\n")
-
+            message = input("* Enter message to encrypt" +
+            "\n* Max message length 120 characters" +
+            "\n* Enter empty message to choose action again\n\n>>> ")
+            message = fr"{message}"
             if len(message) > 120:
+                print("\nMessage is too long.")
+                self.print_lines()
                 continue
 
-            elif message == "back":
-                return None
+            elif message == "":
+                break
 
             else:
-                print("Encrypting message...")
+                self.print_lines()
                 self.encrypted_message = self.crypt.encrypt_message(message, public_key)
                 return self.encrypted_message[0]
 
     def decrypt_previous_message(self, private_key):
-        print("Decrypting message...")
         decrypted = self.crypt.decrypt_message(self.encrypted_message, private_key)
         return decrypted
 
     def select_action(self):
         while True:
+            self.print_lines()
             print("Options:\n"
-            "1 - Encrypt message\n"
-            "2 - Decrypt message\n"
-            "3 - Generate new keys\n"
-            "quit - Quit program\n")
-            action = input("Action: ")
+            "enc - Encrypt message\n"
+            "dec - Decrypt message\n"
+            "showmsg - Show encrypted message\n"
+            "gen - Generate new keys\n"
+            "spub - Show public key\n"
+            "spriv - Show private key\n"
+            "quit - Quit program")
+            self.print_lines()
+            action = input(">>> ")
+            self.print_lines()
 
-            if action == "1":
-                print("\nEncrypted message: "
-                + str(self.encrypt_user_message(self.keygen.get_public_key()))
-                + "\n")
+            if action == "enc":
+                encrypted = self.encrypt_user_message(self.keygen.get_public_key())
+                if encrypted is not None:
+                    print("Encrypted message: "
+                    + str(encrypted))
                 break
 
-            elif action == "2" and (self.encrypted_message is not None):
-                print("\nDecrypted message: "
-                + self.decrypt_previous_message(self.keygen.get_private_key())
-                + "\n")
+            elif action == "dec" and (self.encrypted_message is not None):
+                print("Decrypted message: "
+                + self.decrypt_previous_message(self.keygen.get_private_key()))
                 break
 
-            elif action == "3":
+            elif action == "dec" and (self.encrypted_message is None):
+                print("No encrypted message found.\n")
+
+            elif action == "gen":
+                self.encrypted_message = None
                 self.select_key_bit_size()
                 break
 
             elif action == "quit":
                 self.quit()
+            
+            elif action == "spub":
+                public_key = self.keygen.get_public_key()
+                print(f"Public key: {public_key}")
 
-            print("Choose valid action.\n")
+            elif action == "spriv":
+                private_key = self.keygen.get_private_key()
+                print(f"Private key: {private_key}")
+
+            elif action == "showmsg":
+                if self.encrypted_message is not None:
+                    print(f"Encrypted message:\n> {self.encrypted_message[0]}")
+                else:
+                    print("No encrypted message to found.")
+
+            else:
+                print(">>Choose valid action<<")
 
     def quit(self):
-        print("Shut down...")
+        print("Shutting down")
         quit()
 
-if __name__ == "__main__":
-    ui = UI(Crypt(), KeyGenerator())
-    ui.launch()
-    while True:
-        ui.select_action()
+    def print_lines(self):
+        print("----------------")
+
+ui_obj = UI(Crypt(), KeyGenerator())
